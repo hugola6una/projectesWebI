d35479c9a42973f4786e0225fa7f1c9fbd547948
hugo.laguna <hugo.laguna@students.salle.url.edu>
2023-11-05 19:24:36 +0100

    CreatePlayerView register steps

--- project-base/src/views/CreatePlayerView.vue
+++ project-base/src/views/CreatePlayerView.vue
@@ -1,34 +1,46 @@
 <template>
-   <div class="container">
+  <div class="container">
     <div class="left">
-      <img src="../assets/images/logoRobotArena.png" alt="Logo" class="gameImage">
-      <h1 class="gameTitle">ROBOT</h1>
-      <h1 class="gameTitle">BATTLE</h1>
-      <h2 class="gameSubTitle">ARENA</h2>
+      <div class="left-content">
+        <img src="../assets/images/logoRobotArena.png" alt="Logo" class="gameImage">
+        <h1 class="gameTitle">ROBOT</h1>
+        <h1 class="gameTitle">BATTLE</h1>
+        <h2 class="gameSubTitle">ARENA</h2>
+      </div>
     </div>
     <div class="right">
-      
+      <div class="right-content">
+        <h1 class="createTitle">CREATE PLAYER</h1>
+        <div class="profilePhoto">
+          
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
   .container {
-    display: flex;
-    justify-content: center;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
     align-items: center;
     height: 100vh;
     
   }
 
   .left {
-    text-align: end;
-    justify-content: center;
-    color: white;
-    font-family: 'PressStart2P', sans-serif;
-    background: linear-gradient(to right, #633FC3, #362864);
     height: 100%;
-    width: 100%;
+    text-align: end;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    background: linear-gradient(to right, #633FC3, #362864);
+  }
+
+  .left-content {
+    text-align: center;
+    margin-right: 3rem;
   }
 
   .gameImage {
@@ -44,12 +56,27 @@
   }
 
   .right {
+    height: 100%;
+    text-align: start;
+    color: white;
+    display: flex;
+    align-items: center;
+    background-color: white;
+  }
+
+  .right-content {
     margin-left: 3rem;
+    text-align: center;
     display: flex;
     flex-direction: column;
     align-items: center;
-    height: 100%;
-    width: 100%;
+    justify-content: flex-start;
+    color: #362864;
+  }
+  
+  .createTitle {
+    margin: 0;
+    font-size: 1rem;
   }
 
 </style>
