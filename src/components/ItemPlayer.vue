51dd6afe6fa41420004a28182a0f4e3fded5f70a
arianaiurian <ariana.iurian@students.salle.url.edu>
2023-11-10 19:24:32 +0100

    ranking

---
+++ src/components/ItemPlayer.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+</script>
+
+<template>
+    <article class="player" >
+        <img src="..\assets\images\icons\userIcon.png" alt="playerIcon" class="playerIcon" />
+        <h4>user</h4>
+        <p>155XP</p>
+    </article>
+</template>
+
+<style scoped>
+.player {
+    margin-bottom: 2vh;
+    display: flex;
+    text-align: center;
+    justify-content: space-around;
+    align-items: center;
+    cursor: pointer;
+}
+
+.playerIcon {
+    height: 5vh;
+    width: 5vh;
+}
+
+h4 {
+    font-size: 1.5vmax;
+    font-weight: bold;
+}
+
+p {
+    font-size: 1vmax;   
+}
+
+</style>
---
+++ src/components/RankingSearchContent.vue
@@ -0,0 +1,55 @@
+<script setup>
+    import ItemPlayer from '../components/ItemPlayer.vue';
+    import SearchItem from '../components/SearchItem.vue';
+</script>
+
+<template>
+    <div class="playersContent">
+        <h3>SEARCH</h3>
+        <section class="players">
+            <SearchItem/>
+            <ItemPlayer/>
+            <ItemPlayer/>
+            <ItemPlayer/>
+            <ItemPlayer/>
+            <ItemPlayer/>
+            <ItemPlayer/>
+            <ItemPlayer/>
+            <ItemPlayer/>
+            <ItemPlayer/>
+            <ItemPlayer/>
+        </section>
+    </div>
+</template>
+
+<style scoped>
+
+    .playersContent {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        margin: 2vmax;
+        background-color: white;
+        justify-content: start;
+        text-align: center;
+        align-items: center;
+        max-height: 70vh;
+    }
+
+    .playersContent h3{
+        color: #362864;
+        font-size: 3vmax;
+    }
+    .players {
+        width: 100%;
+        height: 100%;
+        overflow-y: auto;
+    }
+
+
+    @media (max-width: 800px) {
+    .playersContent {
+        max-height: 60vh;
+    }
+}
+</style>
