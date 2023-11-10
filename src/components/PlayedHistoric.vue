c59d8ab885dc9864ea1ab9806ab7ee1c595f92ea
hugo.laguna <hugo.laguna@students.salle.url.edu>
2023-11-10 20:46:35 +0100

    Vamos primer popUp done

---
+++ src/components/PlayedHistoric.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>   
+    <div>
+        <p class="actionDone">ATTACK</p>
+        <p>00:00</p>
+    </div>  
+</template>
+
+<style scoped>
+
+    p {
+        color: #362864;
+        font-size: 1.5vmax;
+    }
+    .actionDone {
+        color: #362864;
+        font-size: 2.5vmax;
+        margin-left: 8vmax;
+        margin-right: 8vmax;
+    }
+</style>
---
+++ src/components/PlayerHistoric.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="playerHistoric">
+        <img src="../assets/images/icons/playerdefault.png" alt="User" class="iUser">
+        <p>Marc</p>
+    </div>
+</template>
+
+<style scoped>
+    .playerHistoric {
+        margin-top: 2vmax;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    }
+
+    p {
+        color: #362864;
+        margin-left: 2vmax;
+        font-size: 2vmax;
+    }
+
+    img {
+        width: 7vmax;
+    }
+</style>
