8c66b09c6f6065b288b634e14fb887ae9e800844
arianaiurian <ariana.iurian@students.salle.url.edu>
2023-11-10 19:39:53 +0100

    petit canvis

--- src/components/RankingSearchContent.vue
+++ src/components/RankingSearchContent.vue
@@ -6,8 +6,10 @@
 <template>
     <div class="playersContent">
         <h3>SEARCH</h3>
-        <section class="players">
+        <section class="search">
             <SearchItem/>
+        </section>
+        <section class="players">
             <ItemPlayer/>
             <ItemPlayer/>
             <ItemPlayer/>
@@ -41,8 +43,16 @@
         font-size: 3vmax;
     }
     .players {
+        margin-top: 0%;
         width: 100%;
-        height: 100%;
+        height: 200%;
+        overflow-y: auto;
+    }
+
+    .search {
+        width: 100%;
+        height: 50%;
+        margin-bottom: 0%;
         overflow-y: auto;
     }
 
