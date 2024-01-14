b1b291ee3491ea5da5c05b6230feb4be93dad23e
pol.cardenal <pol.cardenal@students.salle.url.edu>
2024-01-14 14:50:13 +0100

    Arreglo tonto

--- src/components/SellContent.vue
+++ src/components/SellContent.vue
@@ -63,7 +63,7 @@
 
 <template>
     <section v-if="!showPopup" class="sellContent">
-        <h3>TAP TO SELECT</h3>
+        <p>TAP TO SELECT</p>
         <section class="items">
             <ItemStore v-for="attack in attacks" :key="attack.attack_ID" :attack="attack" :isSelected="checkSelected(attack)" @toggleSelection="toggleSelecion(attack)"></ItemStore>
         </section>
